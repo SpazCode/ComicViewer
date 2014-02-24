a4c0b9568160cfa592a8ee122a9f77162b2e370a
Stuart <spazdragon012@gmail.com>
2014-02-23 23:03:07 -0500

    Getting somewhere finally but realizing that there is an issue with loading rars

--- js/app.js
+++ js/app.js
@@ -1,35 +1,70 @@
 console.log("running\n");
 
-var chosenEntry = null;
-var frame = document.querySelector('#frame');
-var image = document.querySelector('#image');
-var frstb = document.querySelector('#frstbtn');
-var lastb = document.querySelector('#lastbtn');
-var nextb = document.querySelector('#nextbtn');
-var prevb = document.querySelector('#prevbtn');
-var openb = document.getElementById("openbtn");
 var images = [];
 var done = 0;
+var display = 1;
 var dir;
-var curPanel;
-
-// key codes
-var Key = { LEFT: 37, UP: 38, RIGHT: 39, DOWN: 40, 
-A: 65, B: 66, C: 67, D: 68, E: 69, F: 70, G: 71, H: 72, I: 73, J: 74, K: 75, L: 76, M: 77, 
-N: 78, O: 79, P: 80, Q: 81, R: 82, S: 83, T: 84, U: 85, V: 86, W: 87, X: 88, Y: 89, Z: 90};
+var curPanel = 0;
 
 
+//Generic error handler
 function errorHandler(e) {
-  console.error(e);
+  console.log("*** ERROR ***");
+  console.dir(e);
 }
 
-$(document).on("drop", dropHandler);
+// Generic Setting handler for the app
+function saveSettings() {
+  chrome.storage.local.set(['settings']);
+}
 
+function loadSettings() {
 
-$('#openbtn').click( function() {
+}
+
+function init() {
+  console.log("init loaded");
+  navigator.webkitTemporaryStorage.requestQuota(20*1024*1024, function(grantedBytes) {
+    window.webkitRequestFileSystem(window.TEMPORARY, grantedBytes, onInitFs, errorHandler);
+  }, errorHandler);
+}
+
+function onInitFs(fs) {
+  dir = fs.root;
+  $(document).on("dragover", dragOverHandler);
+
+  $(document).on("drop", dropHandler);
+  console.log('onInitFs done, new');
+}
+
+function dragOverHandler(e) {
+  e.preventDefault();
+}
+
+function dropHandler(e) {
+  e.stopPropagation();
+  e.preventDefault();
+
+  if(!e.originalEvent.dataTransfer.files) return;
+  var files = e.originalEvent.dataTransfer.files;
+  var count = files.length;
+
+   if(!count) return;
+
+   //Only one file allowed
+   if(count > 1) {
+     doError("You may only drop one file.");
+     return;
+   }
+
+   handleFile(files[0]);
+ }
+
+$('#openbtn').click( function(e) {
+  e.preventDefault();
   console.log("Choose file\n");
   var accepts = [{
-    extensions: ['zip', 'cbz', 'cbr', 'txt']
+    extensions: ['zip', 'cbz']
   }];
   chrome.fileSystem.chooseEntry({type: 'openFile', accepts: accepts}, function(theEntry) {
     if (!theEntry) {
@@ -39,113 +74,92 @@ $('#openbtn').click( function() {
     // use local storage to retain access to this file
     chrome.storage.local.set({'chosenFile': chrome.fileSystem.retainEntry(theEntry)});
     theEntry.file(function(file) {
-         handleFile(file); 
+      reset;
+      handleFile(file);
     });
   });
 });
 
-function init() {	
-	window.webkitStorageInfo.requestQuota(window.TEMPORARY, 20*1024*1024, function(grantedBytes) {
-		window.webkitRequestFileSystem(window.TEMPORARY, grantedBytes, onInitFs, errorHandler);
-	}, errorHandler);
-
-}
-
-function onInitFs(fs) {
-	dir = fs.root;
-	$(document).on("dragover", dragOverHandler);
-
-	$(document).on("drop", dropHandler);
-	console.log('onInitFs done, new');
-}
-
-function dragOverHandler(e) {
-	e.preventDefault();
-}
-
-
-function dropHandler(e) {
-	e.stopPropagation();
-	e.preventDefault();
-
-	if(!e.originalEvent.dataTransfer.files) return;
-	var files = e.originalEvent.dataTransfer.files;
-	var count = files.length;
- 
- 	if(!count) return;
-
- 	//Only one file allowed
- 	if(count > 1) {
- 		doError("You may only drop one file.");
- 		return;
- 	}
-
- 	handleFile(files[0]);
-}
-
 function handleFile(file) {
-	zip.workerScriptsPath = "lib/";
+  console.log(file);
+  zip.workerScriptsPath = "js/";
 
-	zip.createReader(new zip.BlobReader(file), function(reader) {
-		console.log("did create reader");
-	    reader.getEntries(function(entries) {
-	    	console.log("got entries");
+  zip.createReader(new zip.BlobReader(file), function(reader) {
+    console.log("Created reader.");
+    reader.getEntries(function(entries) {
+      console.log("Got entries.");
 
-	        entries.forEach(function(entry) {
-	        	if(!entry.directory && entry.filename.indexOf(".jpg") != -1) {
-	        		//rewrite w/o a path
-	        		var cleanName = entry.filename;
-	        		if(cleanName.indexOf("/") >= 0) cleanName = cleanName.split("/").pop();
-					dir.getFile(cleanName, {create:true}, function(file) {
-						console.log("Yes, I opened "+file.fullPath);
-		        		images.push({path:file.toURL(), loaded:false})
-						entry.getData(new zip.FileWriter(file), function(e) {
+      entries.forEach(function(entry) {
+        
+        if(!entry.directory && entry.filename.indexOf(".jpg") != -1) {
 
-							done++;
-							var perc = Math.floor(done/images.length*100);
-							var pString = 'Processing images.';
-							pString += '<div class="progress progress-striped active">';
-							pString += '<div class="bar" style="width: '+perc+'%;"></div>';
-							pString += '</div>';
-							$("#alert").html(pString);
+          //rewrite w/o a path
+          var cleanName = entry.filename;
+          if(cleanName.indexOf("/") >= 0) cleanName = cleanName.split("/").pop();
 
-							for(var i=0; i<images.length; i++) {
-								if(images[i].path == file.toURL()) {
-									images[i].loaded = true; 
-									break;
-								}								
-							}
+          dir.getFile(cleanName, {create:true}, function(file) {
+            console.log("Yes, I opened "+file.fullPath);
+            images.push({path:file.toURL(), loaded:false})
+  
+            /*entry.getData(new zip.FileWriter(file), function(e) {
+              done++;
+              var perc = Math.floor((done/images.length)*100);
 
-							if(done == images.length) {
-								//enable buttons
-								$("#prevbtn").on("click",prevPanel);
-								$("#nextbtn").on("click",nextPanel);
-								drawPanel(0);
-							}
-						});
+              for(var i=0; i<images.length; i++) {
+                if(images[i].path == file.toURL()) {
+                  images[i].loaded = true;
+                  break;
+                }
+              }
 
-					},errorHandler);
-
-		        }
-	        },errorHandler);
-	    },errorHandler);
-	}, function(err) {
-		doError("Sorry, but unable to read this as a CBR file.");
-	    console.dir(err);
-	});
-
+              if(done == images.length) loaded;
+              else showProgress(images.length);
+            }, errorHandler);*/
+          }, errorHandler);
+          loaded()
+        }
+      });
+    });
+  }, function(err) {
+    console.dir(err);
+  });
 }
 
-function drawPanel(num) {
-	curPanel = num;
-	$("#image").attr("src",images[num].path);
-	$("#page_count").html("Panel "+(curPanel+1)+" out of "+images.length);
+function loaded() {
+  drawPanel(curPanel);
+}
+
+function showProgress() {
+
 }
 
 function prevPanel() {
-	if(curPanel > 0) drawPanel(curPanel-1);
+  if(curPanel > 0) drawPanel(curPanel-display);
 }
 
 function nextPanel() {
-	if(curPanel+1 < images.length) drawPanel(curPanel+1);
+  if(curPanel+display < images.length) drawPanel(curPanel+display);
 }
+
+function drawPanel(num) {
+  curPanel = num;
+
+  $("#image_display img").each(function( index ) {
+    if (num+index >= images.length || num+index < 0) {
+      $(this).hide();
+    } else {
+      $(this).attr("src",images[num+index].path);
+      $(this).show();
+    }
+  });
+}
+
+function reset() {
+  var images = [];
+  var done = 0;
+  var display = 1;
+  var dir;
+  var curPanel = 0;
+}
+
+window.onload = init();
